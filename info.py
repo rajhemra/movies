37c9aa34e43eda9f3478504387252f5e6a3c0797
rajhemra <rajhamraj45@gmail.com>
2025-02-18 22:48:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,10 +19,10 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7825304144:AAHoL-LiXNYlBKpOpfk1nFt__HGVCU0
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5236284698').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_bebe_dead") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002423117797'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviebala12')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002495162924').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002495162924').split(1)]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rajhamraj45:rajhamraj@hemraj.fygnq.mongodb.net/?retryWrites=true&w=majority&appName=hemraj")
