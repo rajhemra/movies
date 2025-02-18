d195d55ed1d64f8f881a6a8c3e7ef859b0d4f391
rajhemra <rajhamraj45@gmail.com>
2025-02-18 21:02:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rajhamraj45:rajhamraj45@hemraj.fygnq.mongodb.net/?retryWrites=true&w=majority&appName=hemraj")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajhamraj45")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Hemraj")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
